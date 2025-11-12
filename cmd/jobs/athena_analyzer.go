d67507044ff3d1f9ac564414cb5e2742f3cd5ff5
MING-LAPTOP\zhang <zhangming860612@gmail.com>
2025-11-13 01:58:33 +0800

    fix bug

--- cmd/jobs/athena_analyzer.go
+++ cmd/jobs/athena_analyzer.go
@@ -312,7 +312,8 @@ ORDER BY CAST(SUM(f.total_bytes) AS VARCHAR) DESC;
 		if job.Annotations == nil {
 			job.Annotations = make(map[string]string)
 		}
-		job.Annotations[util.AnalyzerReportLocationAnnotation] = resultLocation
+		// 最终地址其实还在manifests.csv的文件内容里面
+		job.Annotations[util.AnalyzerReportLocationAnnotation] = resultLocation + "-manifest.csv"
 		_, err = clientset.BatchV1().Jobs(os.Getenv("MY_POD_NAMESPACE")).Update(ctx, job, metav1.UpdateOptions{})
 		if err != nil {
 			klog.Fatalf("Failed to update job: %v", err)
